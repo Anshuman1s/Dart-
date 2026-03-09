b46a46e312664bb8548104087c484e137c0b5788
Anshuman Shukla <anshumanshukla.12as@gmail.com>
2026-03-09 19:19:47 +0530

    Advance-Collections Function

--- Collections/AdvanceFun.dart
+++ Collections/AdvanceFun.dart
@@ -3,6 +3,9 @@ void main(){
     //map() is used to change every element of a list.
     print(list.map((n)=>n*2));
     // where() selects elements that match a condition.
+    List<int> num = [1,2,3,4,5,6,7,8,9,10];
+    var even = num.where((n)=>n%2==0).toList();
+    print(even);
     // reduce()
     // fold()
     // expand()
