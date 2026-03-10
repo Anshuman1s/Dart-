016c23dd8cbae79603b9df8358744eb0c145eb11
Anshuman Shukla <anshumanshukla.12as@gmail.com>
2026-03-10 21:29:45 +0530

    Every Function

--- Collections/SetsAdvanceFun.dart
+++ Collections/SetsAdvanceFun.dart
@@ -5,5 +5,12 @@
 // isEmpty / isNotEmpty
 
 void main(){
-    
+    // Every
+    //every() checks whether ALL elements in a collection satisfy a condition.
+    //Example List RollNo = [2,4,6,8,10] -> We can see all the rollnumbers are even so this is the condition satisfying over here
+
+    List <int> rollNo = [2,4,6,8,10];
+    var ans =rollNo.every((e)=>e%2==0);
+    print(ans);
+
 }
