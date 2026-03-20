523989319e82c11bb62e9e8d78f9a56a4269c325
Anshuman Shukla <anshumanshukla.12as@gmail.com>
2026-03-20 22:28:20 +0530

    Any

--- Collections/SetsAdvanceFun.dart
+++ Collections/SetsAdvanceFun.dart
@@ -7,11 +7,11 @@
 void main(){
     // Every
     //every() checks whether ALL elements in a collection satisfy a condition.
-    //Example List RollNo = [2,4,6,8,10] -> We can see all the rollnumbers are even so this is the condition satisfying over here
+    //Example List RollNo = [2,4,6,8,10] -> We can see all the rollnumbers are even number so this is the condition satisfying over here
     List <int> rollNo = [2,4,6,8,10];
     var ans =rollNo.every((e)=>e%2==0);
     print(ans);
 
-    // any()
+    // any()-> Any mthod checks
     
 }
