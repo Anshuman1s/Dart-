51900bbcb4f8bf94a6e9cd5d5c2318b78fe90f98
Anshuman Shukla <anshumanshukla.12as@gmail.com>
2026-03-20 22:25:02 +0530

    Update

--- Functions/ArrowFun.dart
+++ Functions/ArrowFun.dart
@@ -2,5 +2,4 @@ void main(){
     int ans = square(5);
     print(ans);
 }
-
 int square(int n)=> n*n;
--- Functions/WithPara.dart
+++ Functions/WithPara.dart
@@ -1,6 +1,7 @@
 void main(){
     greet("Anshuman");
 }
-void greet(username){ // Here dart will treat this username is Dynamic type 
+void greet(username){ 
+    // Here dart will treat this username is Dynamic type 
     print("Hey, $username");
 }
